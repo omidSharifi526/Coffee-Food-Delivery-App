e3d298d3d1ac28683e75768b2eb5b110818ea024
omidSharifi <sharifidavood743@gmail.com>
2021-11-10 19:10:37 +0330

    addCards-row1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom';
 import { MainStyle } from './mainStyle';
 import Landing from './components/LandingPage';
+import Products from './components/Product';
+import {productData} from './components/Product/data'
 
 
 
@@ -13,6 +15,8 @@ function App() {
       <MainStyle/>
       
      <Landing/>
+     <Products heading='تحویل پیتزا در کمترین زمان ممکن' data={productData} />
+     
      </Router>
      </>
     
---
+++ src/components/Product/data.js
@@ -0,0 +1,66 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'پیتزا',
+    name: 'مخصوص اورلاندا',
+    desc:
+      'یکی از بهترین های ما ',
+    price: '$170000',
+    button: 'افزودن به سبد خرید'
+  },
+  {
+    img: product2,
+    alt: 'پیتزا',
+    name: 'قارچ و مرغ',
+    desc:
+      ' پیتزا قارچ و مرغ با بهترین مواد ',
+    price: '$140000',
+    button: 'افزودن به سبد خرید'
+  },
+  {
+    img: product3,
+    alt: 'پیتزا',
+    name: 'قارچ و گوشت ',
+    desc:
+      ' پیتزا قارچ و گوشت با بهترین مواد اولیه',
+    price: '$120000',
+    button: 'افزودن به سبد خرید'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'دونات',
+    name: 'دونات مخصوص',
+    desc:
+      'از بهترین محصولات ما',
+    price: '$140000',
+    button: 'افزودن به سبد خرید'
+  },
+  {
+    img: sweet3,
+    alt: 'بستنی',
+    name: 'های کارامل',
+    desc:
+      'بستنی مخصوص اورلاندا',
+    price: '$12.99',
+    button: 'افزودن به سبد خرید'
+  },
+  {
+    img: sweet1,
+    alt: 'ترکیبی پرررو',
+    name: 'ترکیبی',
+    desc:
+      'یکی از بهترین ترکیبی های پررو ایران',
+    price: '$9.99',
+    button: 'افزودن به سبد خرید'
+  }
+];
