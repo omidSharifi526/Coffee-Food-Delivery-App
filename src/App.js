d2c4e1f54ba3f488c41f561186af21ef5ae6daf7
omidSharifi <sharifidavood743@gmail.com>
2021-11-14 03:15:23 +0330

    addFooter

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/LandingPage';
 import Products from './components/Product';
 import {productData,productDataTwo} from './components/Product/data'
 import OurFeature from './components/OurFeature';
+import Footer from './components/Footer';
 
 
 
@@ -14,12 +15,11 @@ function App() {
    <>
     <Router>
       <MainStyle/>
-      
      <Landing/>
      <Products heading='تحویل پیتزا در کمترین زمان ممکن' data={productData} />
      <OurFeature/>
-     <Products heading='تحویل پیتزا در کمترین زمان ممکن' data={productDataTwo} />
-     
+     <Products heading='با بهترین طعم ها در کنارتان هستیم' data={productDataTwo} />
+     <Footer/>
      </Router>
      </>
     
